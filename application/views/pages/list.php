13f5d89bc8c82e31788abf918ca8d08153849cac
xjnny <you@example.com>
2017-02-23 23:28:34 +1300

    Fixed Up Logic

--- application/views/pages/list.php
+++ application/views/pages/list.php
@@ -7,14 +7,14 @@
 
 <?php if (isset($_SESSION['is_logged_in'])): ?>      
 <?= anchor('todos/add_edit', 'Add'); ?>
+    <?php foreach ($todos as $todo): ?>
+    <div>
+        <span><?= $todo->item; ?></span>&nbsp<?= anchor('todos/add_edit/' . $todo->id, 'Edit'); ?>&nbsp|&nbsp<?= anchor('todos/delete/' . $todo->id, 'Delete'); ?>
+    </div>
+<?php endforeach; ?>
 <?php endif; ?>   
 <?php if (!isset($_SESSION['is_logged_in'])): ?>  
     <h4>You gotta log in to add to your shopping list!</h4>
 <?php endif; ?>   
     
     
-<?php foreach ($todos as $todo): ?>
-    <div>
-        <span><?= $todo->item; ?></span>&nbsp<?= anchor('todos/add_edit/' . $todo->id, 'Edit'); ?>&nbsp|&nbsp<?= anchor('todos/delete/' . $todo->id, 'Delete'); ?>
-    </div>
-<?php endforeach; ?>
