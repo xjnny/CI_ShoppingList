5b29b762b24255984ced54790584414003d1fe9d
xjnny <you@example.com>
2017-02-23 23:09:26 +1300

    added a lil styling

--- application/views/pages/list.php
+++ application/views/pages/list.php
@@ -5,7 +5,14 @@
     <div style="color: red; font-weight: bold;"><?= ($this->session->flashdata('error')); ?></div>
 <?php endif; ?>
 
+<?php if (isset($_SESSION['is_logged_in'])): ?>      
 <?= anchor('todos/add_edit', 'Add'); ?>
+<?php endif; ?>   
+<?php if (!isset($_SESSION['is_logged_in'])): ?>  
+    <h4>You gotta log in to add to your shopping list!</h4>
+<?php endif; ?>   
+    
+    
 <?php foreach ($todos as $todo): ?>
     <div>
         <span><?= $todo->item; ?></span>&nbsp<?= anchor('todos/add_edit/' . $todo->id, 'Edit'); ?>&nbsp|&nbsp<?= anchor('todos/delete/' . $todo->id, 'Delete'); ?>
